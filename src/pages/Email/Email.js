edd50ab125b85ed159b140bb4bca2c1bfc102a6d
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-07 11:00:42 +0900

    :lipstick: Email verification layout 추가
    - email 옵션 선택하여 자동완성 섹션 추가 및 스타일 반영 / 반응형 미적용

--- src/pages/Email/Email.js
+++ src/pages/Email/Email.js
@@ -2,7 +2,31 @@ import React from 'react';
 import './Email.scss';
 
 const Email = () => {
-  return <div className="email">Email</div>;
+  return (
+    <div className="email">
+      <form>
+        <img src="images/nike.png" alt="logo" />
+        <legend>가입 또는 로그인을 위해 이메일을 입력하세요.</legend>
+        <div className="emailContainer">
+          <input type="email" placeholder="이메일" />
+          <p>@</p>
+          <select>
+            <option value="">이메일을 선택하세요</option>
+            <option value="naver.com">naver.com</option>
+            <option value="nate.com">nate.com</option>
+            <option value="daum.net">daum.net</option>
+            <option value="google.com">google.com</option>
+            <option value="hanmail.net">hanmail.net</option>
+          </select>
+        </div>
+        <p>
+          계속 진행하면 나이키의 개인 정보 처리 방침 및 이용약관에 동의하게
+          됩니다.
+        </p>
+        <button type="button">계속</button>
+      </form>
+    </div>
+  );
 };
 
 export default Email;
