f549998e2464d54e0941f47e4f0708e7a22666d2
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-03-07 21:09:38 +0900

    :lipstick: productList 2차 레이아웃 구현

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -17,27 +17,60 @@ const ProductList = () => {
 
   return (
     <div className="productList">
-      <div className="filterContainer">
-        <dl className="filter">
-          <dt>남성 신발</dt>
-          <dd className="category">카테고리</dd>
-          <dd className="gender">남여</dd>
-          <dd className="color">색상</dd>
-          <dd className="color">사이즈</dd>
-        </dl>
+      <div className="productListContainer">
+        <div className="filter">
+          <h3 className="menu">남성 신발</h3>
+          <ul className="category">
+            <li className="categoryMenu">라이프스타일</li>
+            <li className="categoryMenu">조던</li>
+            <li className="categoryMenu">러닝</li>
+            <li className="categoryMenu">농구</li>
+            <li className="categoryMenu">축구</li>
+          </ul>
+          <dl className="filterContainer gender">
+            <dt className="filterTitle">성별</dt>
+            <dd className="man">
+              <input className="checkbox" type="checkbox" />
+              <span>남성</span>
+            </dd>
+            <dd className="woman">
+              <input className="checkbox" type="checkbox" />
+              <span>여성</span>
+            </dd>
+            <dd className="unisex">
+              <input className="checkbox" type="checkbox" />
+              <span>유니섹스</span>
+            </dd>
+          </dl>
+          <dl className="filterContainer color">
+            <dt className="filterTitle">색상</dt>
+            <dd>블랙</dd>
+            <dd>블루</dd>
+            <dd>브라운</dd>
+            <dd>그린</dd>
+            <dd>그레이</dd>
+            <dd>오렌지</dd>
+            <dd>핑크</dd>
+            <dd>퍼플</dd>
+          </dl>
+          <dl className="filterContainer size">
+            <dt className="filterTitle">사이즈</dt>
+            <dd>225</dd>
+            <dd>230</dd>
+            <dd>235</dd>
+            <dd>240</dd>
+            <dd>245</dd>
+          </dl>
+        </div>
         <div className="productListBoard">
-          {productData.map(info => {
+          {productData.map(({ id, img_url, name, price }) => {
             return (
-              <div className="productBox" key={info.id}>
+              <div className="productBox" key={id}>
                 <div className="productBoxImg" />
-                <img
-                  className="productImg"
-                  alt="productImg"
-                  src={info.img_url}
-                />
+                <img className="productImg" alt="productImg" src={img_url} />
                 <div className="productBoxText">
-                  <div className="productName">{info.name}</div>
-                  <div className="productPrice">{info.price} 원</div>
+                  <div className="productName">{name}</div>
+                  <div className="productPrice">{price} 원</div>
                 </div>
               </div>
             );
