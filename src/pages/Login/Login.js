10b76325453858a2e9201d90ee14b1943c7661f6
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-07 19:59:18 +0900

    :memo: MODIFY : input에 className 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
         <img src="images/nike.png" alt="logo" className="logoImage" />
         <legend className="formTitle">비밀번호를 입력하세요.</legend>
         <div className="passwordContainter">
-          <input type="password" placeholder="비밀번호" />
+          <input type="password" className="password" placeholder="비밀번호" />
           <i class="fa-regular fa-eye" />
         </div>
         <p className="searchPassword">비밀번호 찾기</p>
