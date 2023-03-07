6403b446afb9e47030232a4cd8f83047fbe74bff
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-03-07 11:10:04 +0900

    :sparkles: nav bar 1차 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
+import nikeLogo from '../../assets/Nav/nike.png';
+
 const Nav = () => {
-  return <div className="nav">nav</div>;
+  const [navData, setNavData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/navData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setNavData(data);
+      });
+  });
+
+  return (
+    <div className="nav">
+      <div className="navContainer">
+        <Link className="nikeLogo" to="/">
+          <img alt="nikeLogo" src={nikeLogo} />
+        </Link>
+        <div className="navText">
+          {navData.map(info => {
+            return (
+              <div className="navTextList" key={info.id}>
+                <Link to={info.url}>{info.menu}</Link>
+              </div>
+            );
+          })}
+        </div>
+        <div className="iconContainer">
+          <div className="searchContainer">
+            <input className="searchInput" type="text" placeholder="검색" />
+            <i class="fa-solid fa-magnifying-glass" />
+          </div>
+          <Link to="/cart">
+            <i class="fa-solid fa-cart-shopping" />
+          </Link>
+          <Link to="/email">
+            <i class="fa-solid fa-user" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
