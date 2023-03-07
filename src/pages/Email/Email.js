83da5c8de39b97c353583866ebb3b650c835482c
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-07 15:42:20 +0900

    :sparkles: MODIFY : 1차 수정사항 반영
    - option tag 상수 데이터 적용, float 대신 gird 사용, common/scss 원상복구

--- src/pages/Email/Email.js
+++ src/pages/Email/Email.js
@@ -4,29 +4,41 @@ import './Email.scss';
 const Email = () => {
   return (
     <div className="email">
-      <form>
-        <img src="images/nike.png" alt="logo" />
-        <legend>가입 또는 로그인을 위해 이메일을 입력하세요.</legend>
+      <form className="emailForm">
+        <img src="images/nike.png" alt="logo" className="logoImage" />
+        <legend className="formTitle">
+          가입 또는 로그인을 위해 이메일을 입력하세요.
+        </legend>
         <div className="emailContainer">
           <input type="email" placeholder="이메일" />
           <p>@</p>
           <select>
-            <option value="">이메일을 선택하세요</option>
-            <option value="naver.com">naver.com</option>
-            <option value="nate.com">nate.com</option>
-            <option value="daum.net">daum.net</option>
-            <option value="google.com">google.com</option>
-            <option value="hanmail.net">hanmail.net</option>
+            {MAIL_OPTION.map(option => (
+              <option key={option.id} value={option.domain}>
+                {option.domain}
+              </option>
+            ))}
           </select>
         </div>
-        <p>
+        <p className="emailTerms">
           계속 진행하면 나이키의 개인 정보 처리 방침 및 이용약관에 동의하게
           됩니다.
         </p>
-        <button type="button">계속</button>
+        <button type="button" className="emailSubmitBtn">
+          계속
+        </button>
       </form>
     </div>
   );
 };
 
 export default Email;
+
+const MAIL_OPTION = [
+  { id: 1, domain: '이메일을 선택하세요' },
+  { id: 2, domain: 'naver.com' },
+  { id: 3, domain: 'nate.com' },
+  { id: 4, domain: 'daum.net' },
+  { id: 5, domain: 'googole.com' },
+  { id: 6, domain: 'hamail.net' },
+];
