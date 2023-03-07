669df305ea18dc02ad92d1bb2c2770b07bfc0d5a
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-07 17:19:33 +0900

    :sparkles: login page 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Login.scss';
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <form className="loginForm">
+        <img src="images/nike.png" alt="logo" className="logoImage" />
+        <legend className="formTitle">비밀번호를 입력하세요.</legend>
+        <input type="password" placeholder="비밀번호" />
+        <p className="searchPassword">비밀번호 찾기</p>
+        {/* button component 자리 */}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
