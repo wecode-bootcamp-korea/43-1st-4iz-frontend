6afe1a8f3f65b1a6d11c43d87d8608cbd1f02ec7
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-09 19:37:35 +0900

    :sparkles: ADD : 레이아웃 작업

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  return <div className="cart">Cart</div>;
+  return (
+    <div className="cart">
+      <main className="cartContainter">
+        <div>
+          <div className="noticeMembers">
+            <h3>멤버에게 제공되는 무료 배송 서비스</h3>
+            <p>
+              나이키 멤버가 되어 무료배송 서비스를 비롯한 다양한 혜택을
+              누려보세요. 가입하기 또는 로그인
+            </p>
+          </div>
+          <div className="noticeRefund">
+            <p>반품 및 환불 지연 안내</p>
+            <p>자세히 보기</p>
+            <i className="fa-solid fa-xmark" />
+          </div>
+          <section>
+            <h2>장바구니</h2>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Cart;
