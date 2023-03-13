c23c2473195a9cc3ed59a581dde04c2fd2505193
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-03-13 12:02:47 +0900

    :coffin: MODIFY: 불필요한 파일 삭제 (Email, Login, Signup, Authentication)

--- src/Router.js
+++ src/Router.js
@@ -9,10 +9,6 @@ import ProductList from '../src/pages/ProductList/ProductList';
 import ProductDetail from '../src/pages/ProductDetail/ProductDetail';
 import Cart from '../src/pages/Cart/Cart';
 import Order from '../src/pages/Order/Order';
-import Email from '../src/pages/Email/Email';
-import Login from '../src/pages/Login/Login';
-import Signup from '../src/pages/Signup/Signup';
-import Authentication from '../src/pages/Authentication/Authentication';
 
 const Router = () => {
   return (
@@ -24,10 +20,6 @@ const Router = () => {
         <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/order" element={<Order />} />
-        <Route path="/email" element={<Email />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/authentication" element={<Authentication />} />
       </Routes>
       <Footer />
     </BrowserRouter>
