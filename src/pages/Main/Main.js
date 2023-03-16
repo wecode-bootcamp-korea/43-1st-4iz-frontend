5ea72fbd12106983a2b183bb111a0b7fbb752549
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-03-16 17:04:55 +0900

    :lipstick: main style refactoring in progress

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,43 +6,42 @@ import Button from '../../components/Button/Button';
 import './Main.scss';
 
 const Main = () => {
-  // const video = useRef(null);
-
-  // const handleVideoFaster = () => {
-  //   video.current.playbackRate = 1.2;
-  // };
-
-  const main = useRef(null);
-  const mainShoeImg = useRef(null);
-
-  const scrollEvent = () => {
-    //test
-  };
-
   return (
-    <div className="main" ref={main} onScroll={scrollEvent}>
-      <img
-        src="/images/mainBgWhite.png"
-        className="mainBgImg"
-        alt="mainBgImg"
-      />
-      <img
-        src="/images/mainShoeImg.png"
-        className="mainShoeImg"
-        alt="mainShoeImg"
-        ref={mainShoeImg}
-      />
-      <div className="mainBox">
-        {/* <video
-          src="/videos/mainVid.mp4"
-          ref={video}
-          onPlay={handleVideoFaster}
-          autoPlay
-          muted
-          loop
-          playsinline
-        /> */}
-        <div className="mainVidDescription">
+    <div className="main">
+      <section className="mainContentsWrap">
+        <img
+          src="/images/mainBgWhite.png"
+          className="mainBgImg"
+          alt="mainBgImg"
+        />
+        <div className="mainContentsAnime">
+          <div className="card">
+            <div className="circle" />
+            <div className="content">
+              <h2># MAXXED OUT</h2>
+              <h4>시대를 초월한 에어맥스</h4>
+              <br />
+              <p>
+                나이키 에어맥스. 수많은 실패, 그리고 무한한 가능성. 시대를
+                초월한 에어맥스 스타일과 새로운 MAXXED OUT 팩을 만나 보세요.
+              </p>
+              <br />
+              <Link to="/productDetail">구매하기</Link>
+              <br />
+              <br />
+              <p>NIKE AIRMAX</p>
+            </div>
+            <img
+              src="/images/mainShoeImg.png"
+              className="mainShoeImg"
+              alt="mainShoeImg"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="mainContainer">
+        <div className="mainDescription">
           <h6 className="descriptionSubtitle">에어맥스 with 지올팍</h6>
           <h2 className="descriptionTitle">시대를 초월한 에어맥스</h2>
           <p className="descriptionText">
@@ -55,55 +54,9 @@ const Main = () => {
             <Button className="button" text="구매하기" />
           </Link>
         </div>
-        {/* <img className="mainImg" alt="mainImg" src="/images/main.png" /> */}
-        {/* <div className="mainVidDescription">
-          <h2 className="descriptionTitle">A FEEL FOR EVERY YOU</h2>
-          <p className="descriptionText">
-            몸과 마음이 좋아하는 나만의 다양한 움직임을 시작해보세요.
-          </p>
-          <p className="descriptionText">
-            머리부터 발끝까지 모든 감각들을 깨워, 기분 좋은 하루를 보낼 수 있는
-            에너지를 만들 수 있어요.
-          </p>
-          <p className="descriptionText">
-            지금 나를 위한 모든 가능성을 느껴보세요.
-          </p>
-          <div className="buttonContainer">
-            <Link to="/productList">
-              <Button className="button" text="자세히 보기" />
-            </Link>
-            <Link to="/productDetail">
-              <Button className="button" text="구매하기" />
-            </Link>
-          </div>
-        </div> */}
-      </div>
+      </section>
 
-      <div className="animationTest">
-        <div className="card">
-          <div className="circle" />
-          <div className="content">
-            <h2>Nike</h2>
-            <h4>Lupinek Flyknit</h4>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
-            </p>
-            <br />
-            <Link to="">Buy Now</Link>
-            <br />
-            <br />
-            <p>_man_mohan_25</p>
-          </div>
-          <img
-            src="/images/mainShoeImg.png"
-            className="mainShoeImg"
-            alt="mainShoeImg"
-            ref={mainShoeImg}
-          />
-        </div>
-      </div>
+      <section className="mainDragSection">drag</section>
     </div>
   );
 };
